a69462e62a6d834beb3606ebdf8d9668c430ad05
kronosGR <geo.elgeo@gmail.com>
2022-06-01 12:11:34 +0200

    fixed tsc errors

--- src/Components/Toolbar/ColorPicker/usePaint.tsx
+++ src/Components/Toolbar/ColorPicker/usePaint.tsx
@@ -1,21 +1,21 @@
 import React, { Ref, useEffect } from 'react';
 
-export interface ICanvas {
-  canvas: Ref<HTMLCanvasElement>;
-}
-
-const usePaint = (canvas: ICanvas, size: number) => {
+const usePaint = (canvas: HTMLCanvasElement, size: number) => {
   useEffect(() => {
-    const context = canvas.current.context('2d');
-    context.rext(0, 0, size, size);
+    const context: CanvasRenderingContext2D = canvas.getContext(
+      '2d'
+    ) as CanvasRenderingContext2D;
+    if (context !== undefined) {
+      context.rect(0, 0, size, size);
 
-    const gradient = context.createLinearGradient(0, 0, size, 0);
-    for (let i = 0; i <= 360; i += 12) {
-      gradient.addColorStop(i / 360, `hsl(${i}, 100%, 50%)`);
+      const gradient = context?.createLinearGradient(0, 0, size, 0);
+      for (let i = 0; i <= 360; i += 12) {
+        gradient?.addColorStop(i / 360, `hsl(${i}, 100%, 50%)`);
+      }
+
+      context.fillStyle = gradient;
+      context?.fill();
     }
-
-    context.fillStyle = gradient;
-    context.fill();
   }, [canvas]);
 };
 
