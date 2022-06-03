3dedfb4acb9bc25c26b327b83a0f0eaabdf7e023
kronosGR <geo.elgeo@gmail.com>
2022-06-03 12:05:17 +0200

    updated usePaint

--- src/Components/Toolbar/ColorPicker/usePaint.tsx
+++ src/Components/Toolbar/ColorPicker/usePaint.tsx
@@ -1,14 +1,17 @@
 import React, { Ref, useEffect } from 'react';
+import config from '../../../Constants/config';
 
-const usePaint = (canvas: React.MutableRefObject<HTMLCanvasElement>, size: number) => {
+const { squareSize, barSize } = config;
+
+const usePaint = (canvas: React.MutableRefObject<HTMLCanvasElement>) => {
   useEffect(() => {
     const context: CanvasRenderingContext2D = canvas.current.getContext(
       '2d'
     ) as CanvasRenderingContext2D;
     if (context !== undefined) {
-      context.rect(0, 0, size, 30);
+      context.rect(0, 0, squareSize, barSize);
 
-      const gradient = context?.createLinearGradient(0, 0, size, 0);
+      const gradient = context?.createLinearGradient(0, 0, squareSize, 0);
       for (let i = 0; i <= 360; i += 12) {
         gradient?.addColorStop(i / 360, `hsl(${i}, 100%, 50%)`);
       }
