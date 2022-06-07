7e8abce74735bb523c52c0200800dead9c7594fe
kronosGR <geo.elgeo@gmail.com>
2022-06-07 12:08:23 +0200

    added wrapper divs

--- src/Components/Toolbar/ColorPicker/ColorPicker.tsx
+++ src/Components/Toolbar/ColorPicker/ColorPicker.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+import config from '../../../Constants/config';
 import ColorBar from './ColorBar';
 import Modal from './Modal';
 
+const { squareSize, barSize } = config;
 interface Props {
   top: number;
   left: number;
@@ -22,6 +24,21 @@ const ColorPickerWrapper = styled.div.attrs<ItemProps>((itemProps) => ({
   position: absolute;
 `;
 
+export const PickerOuter = styled.div`
+  width: ${squareSize + 20}px;
+  display: grid;
+  border-radius: 2px;
+  background: white;
+  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.3);
+`;
+
+export const PickerInner = styled.div`
+  display: grid;
+  grid-template-rows: ${squareSize + 20}px 20px 1fr;
+  align-items: center;
+  justify-items: center;
+`;
+
 const Button = styled.div`
   padding: 1rem;
   border: 1px solid black;
@@ -37,7 +54,10 @@ export default function ColorPicker({ top, left }: Props) {
     <ColorPickerWrapper top={top} left={left}>
       <Button onClick={() => setShow(true)}>Show</Button>
       <Modal modal={modal} show={show} onClose={() => setShow(false)}>
-        <ColorBar top={200} left={100} />
+        <PickerOuter>
+          <PickerInner></PickerInner>
+          <ColorBar top={200} left={100} />
+        </PickerOuter>
       </Modal>
     </ColorPickerWrapper>
   );
